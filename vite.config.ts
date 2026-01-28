6faec30d0040fb57f7cfc6f3d4452d6f8161f7fb
Naveen chauhan <143591216+nav380@users.noreply.github.com>
2026-01-28 16:14:47 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// __dirname points to the root folder (outside client)
 export default defineConfig({
-  root: path.resolve(__dirname, "client"), // source folder
-  base: "./", // relative paths for SPA
+  root: path.resolve(__dirname, "client"),  // source folder
+  base: "./",                                // relative paths in build
   plugins: [react(), tailwindcss()],
   publicDir: path.resolve(__dirname, "client", "public"),
   build: {
     outDir: path.resolve(__dirname, "client", "dist"), // output inside client
     emptyOutDir: true,
+    chunkSizeWarningLimit: 2000
   },
   resolve: {
     alias: {
@@ -20,5 +20,4 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  
 });
